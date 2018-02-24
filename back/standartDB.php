1c2da8c75a0d93417f82d044643b97c55d308df7
earlold <dimonsuper010@gmail.com>
2018-02-24 21:50:10 +0200

    Added back-end functions. (reverted from commit 5b52e5baa4c7f7dba2e788fc848da3eadb3f25b9)

--- back/standartDB.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$mysqli = new mysqli("localhost", "root", "root", "lab8db");
-  if (mysqli_connect_errno()) {
-    echo "Подключение невозможно: ".mysqli_connect_error();
-  }
-  $mysqli->query("SET NAMES utf8 COLLATE utf8_general_ci");
