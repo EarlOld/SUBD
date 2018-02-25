f50ad9f0d5b69f56a332240ef46070540ae8dc11
earlold <dimonsuper010@gmail.com>
2018-02-25 12:07:15 +0200

    Added form for insert row to database.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Table, Button, Popconfirm, Modal } from 'antd'
+import { Table, Button, Popconfirm, Modal, Form, Input } from 'antd'
 import Header from '../Header'
 import EditableCell from './EditableCell'
+const FormItem = Form.Item
 
 
 class App extends Component {
@@ -11,12 +12,12 @@ class App extends Component {
     this.state = {
       data: [],
       columns: [],
-      visible: false
+      visible: false,
+      newDataItem: {}
     }
   }
-
-  componentDidMount() {
-    fetch(`http://zhatk.loc/get.php?table=${this.props.routeParams.table}`, {
+  getData(nextProps) {
+    fetch(`http://zhatk.loc/get.php?table=${nextProps.routeParams.table}`, {
     }).then(response => response.json()).then(data => {
       const columns = Object.keys(data[0]).map(item => {
         return {
@@ -62,55 +63,18 @@ class App extends Component {
           )
         }
       })
-      this.setState({ data, columns })
+      this.setState({ data: data.sort((a, b) => a['Код'] - b['Код']), columns })
     })
+  }
+
+  componentDidMount() {
+    this.getData(this.props)
   }
 
   componentWillReceiveProps(nextProps) {
-    fetch(`http://zhatk.loc/get.php?table=${nextProps.routeParams.table}`, {
-    }).then(response => response.json()).then(data => {
-      const columns = Object.keys(data[0]).map(item => {
-        return {
-          title: item,
-          dataIndex: item,
-          key: item,
-          render: (text, record) => this.renderColumns(text, record, item)
-        }
-      })
-
-      columns.push({
-        title: 'operation',
-        dataIndex: 'operation',
-        render: (text, record) => {
-          const { editable } = record
-
-          return (
-            <div className='editable-row-operations'>
-              {
-                editable ?
-                  <span>
-                    <Button onClick={() => this.save(record['Код'])}>Save</Button>
-                    <Popconfirm title='Sure to cancel?' onConfirm={() => this.cancel(record['Код'])}>
-                      <Button>Cancel</Button>
-                    </Popconfirm>
-                  </span>
-                  : <Button onClick={() => this.edit(record['Код'])}>Edit</Button>
-              }
-              {
-                this.state.data.length > 1 ?
-                  (
-                    <Popconfirm title='Sure to delete?' onConfirm={() => this.onDelete(record['Код'])}>
-                      <Button>Delete</Button>
-                    </Popconfirm>
-                  ) : null
-              }
-            </div>
-          )
-        }
-      })
-      this.setState({ data, columns })
-    })
+    this.getData(nextProps)
   }
+
   handleChange(value, key, column) {
     debugger
     const newData = this.state.data
@@ -177,13 +141,41 @@ class App extends Component {
     this.setState({ data: dataSource.filter(item => item['Код'] !== key) })
   }
   showModal = () => {
+    const newDataItem = {}
+
+    for (let i = 0; i < this.state.columns.length; i++) {
+      if (i > 0 && i < this.state.columns.length - 1) {
+        newDataItem[this.state.columns[i].key] = null
+      }
+    }
     this.setState({
-      visible: true
+      visible: true,
+      newDataItem
     })
   }
   handleOk = (e) => {
+    const data = {
+      ...this.state.newDataItem,
+      table: this.props.routeParams.table
+    }
+
+    fetch('http://zhatk.loc/add.php', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      body: `arrObjects=${JSON.stringify(data)}`
+    }).then(() => this.getData(this.props))
     this.setState({
       visible: false
+    })
+  }
+  handleChangeSaveForm = (e) => {
+    const { newDataItem } = this.state
+
+    newDataItem[e.target.name] = e.target.value
+    this.setState({
+      newDataItem
     })
   }
   handleCancel = () => this.setState({ visible: false })
@@ -208,9 +200,20 @@ class App extends Component {
           onOk={this.handleOk}
           onCancel={this.handleCancel}
         >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
+          {
+            this.state.columns.map((item, index) => {
+              if (index > 0 && index < this.state.columns.length - 1) {
+                return (
+                  <FormItem
+                    key={index}
+                    label={item.key}
+                  >
+                    <Input value={this.state.newDataItem[item.key]} name={item.key} onChange={this.handleChangeSaveForm} />
+                  </FormItem>
+                )
+              }
+            })
+          }
         </Modal>
         <Header />
         <Button className='editable-add-btn' onClick={this.showModal}>Add row</Button>
